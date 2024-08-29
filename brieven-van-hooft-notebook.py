046a585e25e66f172aa9f87a499b4754f2ddcf55
Maarten van Gompel <proycon@anaproy.nl>
2024-08-29 10:17:22 +0200

    typo

--- brieven-van-hooft-notebook.py
+++ brieven-van-hooft-notebook.py
@@ -49,7 +49,7 @@ def __(mo):
     mo.md(f"""### Obtaining the data
 
     We first obtain the data by downloading the original texts of the three books
-    from DBNL, and by downloading the STAM model from Zenodo. Then will will load the data into memory. All this may take a
+    from DBNL, and by downloading the STAM model from Zenodo. Then we will load the data into memory. All this may take a
     while. Please wait until both these tasks are marked as done below:
     """)
     return
